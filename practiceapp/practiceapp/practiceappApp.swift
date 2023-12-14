ea3f623d06962512e226dc23709dccc77bbf50c0
Silvanatekia <147284865+Silvanatekia@users.noreply.github.com>
2023-12-14 21:41:39 +0000

    Add files via upload

---
+++ practiceapp/practiceapp/practiceappApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceappApp.swift
+//  practiceapp
+//
+//  Created by Silvana Tekia on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct practiceappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
