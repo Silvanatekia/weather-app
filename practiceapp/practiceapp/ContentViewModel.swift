70bc4bcdb7d0e70db5f80acf0288abbc13b44a22
Silvanatekia <147284865+Silvanatekia@users.noreply.github.com>
2023-12-19 23:29:55 +0000

    Add files via upload

--- practiceapp/practiceapp/ContentViewModel.swift
+++ practiceapp/practiceapp/ContentViewModel.swift
@@ -7,41 +7,10 @@
 
 import Foundation
 
-class WeatherViewModel {
-    var weather: Bindable<WeatherModel?> = Bindable(nil)
-
+class ViewModel: ObservableObject {
+   
     func fetchWeather() {
         let apiUrl = "api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=ff50b7e3d20ecc1bb9e26105ee8c3caf"
-
-        guard let url = URL(string: apiUrl) else { return }
-
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data, error == nil else { return }
-
-            do {
-                let londonWeatherData = try JSONDecoder().decode(LondonWeatherData.self, from: data)
-                let weatherModel = WeatherModel
-                    temperature: londonWeatherData.main.temp,
-                    condition: londonWeatherData.weather.first?.description ?? "N/A"
-                )
-                self.weather.value = weatherModel
-            } catch {
-                print("Error decoding London weather data: \(error.localizedDescription)")
-            }
-        }.resume()
     }
+    
 }
-struct LondonWeatherData: Codable {
-    let main: Main
-    let weather: [Weather]
-
-    struct Main: Codable {
-        let temp: Double
-    }
-
-    struct Weather: Codable {
-        let description: String
-    }
-}
-
-
